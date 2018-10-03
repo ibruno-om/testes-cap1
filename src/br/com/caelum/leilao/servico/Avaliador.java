0cb2b4e0621fb8654cc1733e4ceaf1bdc4d9f3a2
Iago Bruno <ibruno.om@gmail.com>
2018-10-03 13:23:07 -0300

    Primeiro Commit

---
+++ src/br/com/caelum/leilao/servico/Avaliador.java
@@ -0,0 +1,44 @@
+package br.com.caelum.leilao.servico;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+
+/**
+ * Serviço de avaliação de lance.
+ * 
+ * @author iago.bruno
+ *
+ */
+public class Avaliador {
+
+	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
+	private double menorDeTodos = Double.POSITIVE_INFINITY;
+
+	/**
+	 * Classifica o maior e menor valor de lance
+	 * 
+	 * @param leilao
+	 *            {@link Leilao} que será avaliado.
+	 */
+	public void avalia(Leilao leilao) {
+
+		for (Lance lance : leilao.getLances()) {
+			if (lance.getValor() > maiorDeTodos) {
+				maiorDeTodos = lance.getValor();
+			}
+
+			if (lance.getValor() < menorDeTodos) {
+				menorDeTodos = lance.getValor();
+			}
+		}
+	}
+
+	public double getMaiorLance() {
+		return maiorDeTodos;
+	}
+
+	public double getMenorLance() {
+		return menorDeTodos;
+	}
+
+}
---
+++ src/br/com/caelum/leilao/teste/TesteAvaliador.java
@@ -0,0 +1,47 @@
+package br.com.caelum.leilao.teste;
+
+import org.junit.Test;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Usuario;
+import br.com.caelum.leilao.servico.Avaliador;
+import junit.framework.Assert;
+
+/**
+ * Classe de Teste do Serviço de Avaliação de Lance
+ * 
+ * @author iago.bruno
+ *
+ */
+public class TesteAvaliador {
+
+	@Test
+	public void testeAvaliarLance() {
+		
+		// parte 1: cenario
+		Usuario joao = new Usuario("João");
+		Usuario jose = new Usuario("José");
+		Usuario maria = new Usuario("Maria");
+		
+		Leilao leilao = new Leilao("Playstation 4 Novo");
+		
+		leilao.propoe(new Lance(joao, 250.0));
+		leilao.propoe(new Lance(jose, 300.0));
+		leilao.propoe(new Lance(maria, 400.0));
+		
+		// parte 2: acao
+		Avaliador leiloeiro = new Avaliador();		
+		leiloeiro.avalia(leilao);
+		
+		// parte 3: validacao
+		
+		double maiorEsperado = 400;
+		double menorEsperado = 250;
+		
+		Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+		Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+		
+	}
+
+}
